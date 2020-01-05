3ed9543f681bd0b9326b49a062af15a4b414685d
soerman <kescha356@gmail.com>
2020-01-05 22:12:40 +0100

    added gitignore

--- dotmatrix1/dotmatrix1.ino
+++ dotmatrix1/dotmatrix1.ino
@@ -2,6 +2,7 @@
 #include <MD_Parola.h>
 #include <MD_MAX72xx.h>
 #include <SPI.h>
+#include <secret.h>
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
 #define MAX_DEVICES 8
 #define CLK_PIN   D7
@@ -33,8 +34,8 @@ float bme_temp, bme_humidity, bme_pressure;
 #include <NTPClient.h>
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
-const char *ssid     = "Hotpoint";
-const char *password = "6161616161";
+const char *ssid     = "SSID";
+const char *password = "PSK";
 const long utcOffsetInSeconds = 3600;
 //char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 char daysOfTheWeek[7][12] = {"SON", "MON", "DIE", "MIT", "DON", "FRE", "SAM"};
@@ -50,7 +51,7 @@ int counter;
 
 void setup(void)
 {
-delay(5000); 
+//delay(5000);  //for DEBUG 
 //Serial////////////////////////////////
 Serial.begin(9600);
 while(!Serial); 
